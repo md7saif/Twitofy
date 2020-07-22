6781b937477750d27ef93576bff188d2a6dad51f
Md. Saif <md7.saif@gmail.com>
2020-07-22 18:29:09 +0100

    Commit 2, 22.07.2020

--- app/src/main/java/com/m7/twitofy/MainActivity.java
+++ app/src/main/java/com/m7/twitofy/MainActivity.java
@@ -3,6 +3,9 @@ package com.m7.twitofy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_screen);
+
+        Button click_here_btn = (Button) findViewById(R.id.click_here_btn);
+        click_here_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "I think this works :)", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
