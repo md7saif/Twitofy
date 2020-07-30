6980b4ce75117523800e41b98418a7c4370cc511
Md. Saif <md7.saif@gmail.com>
2020-07-30 10:34:20 +0100

    Commit 4, 30.07.2020

---
+++ app/src/main/java/com/m7/twitofy/PagerAdapter.java
@@ -0,0 +1,25 @@
+package com.m7.twitofy;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class PagerAdapter extends FragmentPagerAdapter {
+
+
+    public PagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
--- app/src/main/java/com/m7/twitofy/SecondActivity.java
+++ app/src/main/java/com/m7/twitofy/SecondActivity.java
@@ -1,6 +1,7 @@
 package com.m7.twitofy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Context;
 import android.os.Bundle;
@@ -8,13 +9,16 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TableLayout;
 import android.widget.TextView;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 public class SecondActivity extends AppCompatActivity {
 
     String text_to_search;
     EditText word_input;
-    TextView display_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +26,12 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.second_screen);
 
         word_input = (EditText) findViewById(R.id.word_input);
-        display_text = (TextView) findViewById(R.id.display_text);
 
         Button go_btn = (Button) findViewById(R.id.go_btn);
         go_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 text_to_search = word_input.getText().toString();
-                display_text.setText(text_to_search);
 
                 // Hides keyboard
                 InputMethodManager inputManager = (InputMethodManager)
@@ -39,5 +41,13 @@ public class SecondActivity extends AppCompatActivity {
                         InputMethodManager.HIDE_NOT_ALWAYS);
             }
         });
+
+
+        TabLayout tab_analytics = findViewById(R.id.tab_analytics);
+        TabItem graph_tab = findViewById(R.id.graph);
+        TabItem tweets_tab = findViewById(R.id.tweets);
+        ViewPager2 viewPager = findViewById(R.id.viewPager);
+
+
     }
 }
